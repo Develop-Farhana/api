166c09a7c245ed1d1f7196fb61a178730785f544
Develop-Farhana <Coderfarhana99@gmail.com>
2024-06-12 12:27:41 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name'          => $this->name,
             'description'   => $this->detail,
             'price'         => $this->price,
-            'stock'         => $this->stock,
-            'discount'      => $this->discount,
+            'stock'         => $this->stock == 0 ? ' Out Of Stock': $this->stock,
+            'discounts'      => $this->discounts,
+            'totalPrice'      => round(( 1-($this->discounts/100) )*$this->price,2),
+            'rating'         =>$this->review->count() > 0 ? round($this->review->sum('star')/
+            $this->review->count(),2):'No rating Yet',
+            'href' => [
+                'review' => route('reviews.index', $this->id)
+            ],
 
         ];
     }
